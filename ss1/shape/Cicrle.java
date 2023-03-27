610453d3e887f9f918f01f29143c92f0512170aa
DinhDuy170703 <dinhduynguyen0707@gmail.com>
2023-03-27 20:58:47 +0700

    fixbaitap

--- ss1/shape/Cicrle.java
+++ ss1/shape/Cicrle.java
@@ -10,9 +10,9 @@ public class Cicrle {
         double acreage = (number1 * number1) * 3.14;
         System.out.println("diện tích hình tròn là: " + acreage);
 
-        System.out.println("nhập c: ");
+        System.out.println("nhập cạnh c: ");
         double number2 = sc.nextDouble();
-        double perimeter = (number2 * 2) * 3.14;
+        double perimeter = number2 * 2 * 3.14;
         System.out.println("chu vi hình tròn là: " + perimeter);
 
     }
