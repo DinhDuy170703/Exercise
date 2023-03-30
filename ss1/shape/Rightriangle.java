6abf62d57271ac5c97a200dfd19c749f019a5eef
DinhDuy170703 <dinhduynguyen0707@gmail.com>
2023-03-30 20:55:30 +0700

    fixbaitap

--- ss1/shape/Rightriangle.java
+++ ss1/shape/Rightriangle.java
@@ -6,9 +6,9 @@ public class Rightriangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("nhap canh a: ");
-        double a = sc.nextDouble();
+        double a = Double.parseDouble(sc.nextLine());
         System.out.println("nhap canh b: ");
-        double b = sc.nextDouble();
+        double b = Double.parseDouble(sc.nextLine());
 
         double c = Math.sqrt(Math.pow(a, 2 )+ Math.pow(b, 2));
         System.out.println("canh c con lai la: "+ c);
