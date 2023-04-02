cadf0cc01dbb88c572d34118c384d3d97eeb4928
DinhDuy170703 <dinhduynguyen0707@gmail.com>
2023-04-02 18:25:20 +0700

    fixbaitap

--- ss2/TinhSoNgay.java
+++ ss2/TinhSoNgay.java
@@ -1,4 +1,5 @@
 package ss2;
+
 import java.util.Scanner;
 
 public class TinhSoNgay {
@@ -7,13 +8,13 @@ public class TinhSoNgay {
         System.out.println("nhập số ngày");
         int number1 = Integer.parseInt(sc.nextLine());
 
-       switch (number1){
-           case 1 :
-               System.out.println("tháng 1 có 31 ngày");
-               break;
-           case 2 :
-               System.out.println("tháng 2 có 30 hoặc 31 ngày");
-               break;
-       }
+        switch (number1) {
+            case 1,3,5,7,8:
+                System.out.println("tháng 1 có 31 ngày");
+                break;
+            case 2:
+                System.out.println("tháng 2 có 28 hoặc 29 ngày");
+                break;
+        }
     }
 }
