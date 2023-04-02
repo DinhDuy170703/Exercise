299ec5ab50ef06c7e2ecd8f72f1ff9500c9f5994
DinhDuy170703 <dinhduynguyen0707@gmail.com>
2023-04-02 20:35:52 +0700

    fixbaitap

--- ss2/NumberOfDay.java
+++ ss2/NumberOfDay.java
@@ -2,18 +2,19 @@ package ss2;
 
 import java.util.Scanner;
 
-public class TinhSoNgay {
+public class NumberOfDay {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("nhập tháng");
         int month = Integer.parseInt(sc.nextLine());
 
         switch (month) {
-            case 1 :
-            case 3 :
-            case 5 :
-            case 7 :
-            case 8 :
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 11:
                 System.out.println("tháng có 31 ngày");
                 break;
             case 2:
@@ -25,6 +26,9 @@ public class TinhSoNgay {
             case 10:
             case 12:
                 System.out.println("tháng có 30 ngày");
+                break;
+            default:
+                System.out.println("không có tháng này");
                 break;
         }
     }
